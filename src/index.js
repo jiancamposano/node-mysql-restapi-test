9c0ec0996aee1f192ca2e9521485555de91fe9c6
jiancamposano <jiancamposano17@gmail.com>
2025-02-10 02:08:37 +0100

    new port 5000

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import app from './app.js'
 
 config();
 
-export const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
-console.log(`se esta ejecutando en el PUERTO ${PORT}` );
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
