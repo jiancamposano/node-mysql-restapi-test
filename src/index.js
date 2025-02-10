36e41d4d4378012ca35d519282d4e79fe5339188
jiancamposano <jiancamposano17@gmail.com>
2025-02-10 02:00:12 +0100

    port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import {PORT} from './config.js'
+
 import app from './app.js'
+
+export const PORT = process.env.PORT || 3000;
 
 app.listen(PORT);
 console.log(`se esta ejecutando en el PUERTO ${PORT}` );
